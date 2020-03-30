4c63d253facebe4fe4ae578dc151e9f33e0d6409
Sven Gato Redsun <svengato@svengato.com>
2020-03-30 10:54:55 -0600

    Correct spelling and grammar mistakes

--- arapheno/phenotypedb/tables.py
+++ arapheno/phenotypedb/tables.py
@@ -14,9 +14,9 @@ class ReducedPhenotypeTable(tables.Table):
     """
     name = tables.LinkColumn("phenotype_detail", args=[A('id')], text=lambda record: record.name, verbose_name="Phenotype Name", order_by="name")
     to = tables.Column(accessor="to_term.name", verbose_name="Trait Ontology (TO)", order_by="to_term.name")
-    eo = tables.Column(accessor="eo_term.name", verbose_name="Environmental Ontoloy (EO)", order_by="eo_term.name")
+    eo = tables.Column(accessor="eo_term.name", verbose_name="Environmental Ontology (EO)", order_by="eo_term.name")
     uo = tables.Column(accessor="uo_term.name", verbose_name="Unit Ontology (UO)", order_by="uo_term.name")
-    values = tables.Column(accessor="num_values", verbose_name="# values", order_by="num_values")
+    values = tables.Column(accessor="num_values", verbose_name="# Values", order_by="num_values")
 
     ''' # Too many links in table might be confusing
     def render_to(self,record):
